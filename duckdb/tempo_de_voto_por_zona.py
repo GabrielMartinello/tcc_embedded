5e6b69445f91ce2d11f664aa77c85e541996f751
Gabriel Martinello <martinellogabriel@gmail.com>
2025-05-20 14:33:31 -0300

    ajuste querys

--- duckdb/tempo_de_voto_por_zona.py
+++ duckdb/tempo_de_voto_por_zona.py
@@ -21,48 +21,80 @@ FIM_VOTO = 'O voto do eleitor foi computado'
 # cada nova ocorrência representa um novo voto.
 # Calcula o tempo médio por zonas
 query_vote_data = f"""
-    WITH base AS (
-        SELECT *,
-            SUM(CASE WHEN event_description = '{ANCORA}' THEN 1 ELSE 0 END) 
-            OVER (PARTITION BY event_date, uf, filename ORDER BY event_timestamp) AS vote_id
-        FROM read_csv_auto('{csv_path}')
-    ),
-    votos_filtrados AS (
-        SELECT
-            event_timestamp,
-            event_description,
-            uf,
-            event_date,
-            filename,
-            city_code,
-            zone_code,
-            section_code,
-            vote_id
-        FROM base
-        WHERE event_description IN ('{ANCORA}', '{FIM_VOTO}')
-    ),
-    votos_processados AS (
-        SELECT
-            uf,
-            event_date,
-            zone_code,
-            vote_id,
-            MIN(CASE WHEN event_description = '{ANCORA}' THEN event_timestamp END) AS inicio_voto,
-            MAX(CASE WHEN event_description = '{FIM_VOTO}' THEN event_timestamp END) AS fim_voto
-        FROM votos_filtrados
-        GROUP BY uf, event_date, zone_code, vote_id
-        HAVING inicio_voto IS NOT NULL AND fim_voto IS NOT NULL AND fim_voto > inicio_voto
-    )
-    SELECT
+WITH dados AS (
+    SELECT 
+        *,
+        CAST(event_timestamp AS TIMESTAMP) AS ts
+    FROM read_csv_auto('{csv_path}')
+    WHERE event_description IN ('{ANCORA}', '{FIM_VOTO}')
+),
+inicio AS (
+    SELECT 
+        ts AS inicio_voto,
         uf,
+        event_date,
+        filename,
+        city_code,
         zone_code,
-        AVG(EXTRACT(epoch FROM fim_voto - inicio_voto)) AS tempo_medio_voto_segundos,
-        COUNT(*) AS total_votos
-    FROM votos_processados
-    GROUP BY uf, zone_code
-    ORDER BY tempo_medio_voto_segundos DESC
-    LIMIT 10    
+        section_code,
+        ROW_NUMBER() OVER (
+            PARTITION BY event_date, uf, filename, city_code, zone_code, section_code
+            ORDER BY ts
+        ) AS vote_id
+    FROM dados
+    WHERE event_description = '{ANCORA}'
+),
+fim AS (
+    SELECT 
+        ts AS fim_voto,
+        uf,
+        event_date,
+        filename,
+        city_code,
+        zone_code,
+        section_code,
+        ROW_NUMBER() OVER (
+            PARTITION BY event_date, uf, filename, city_code, zone_code, section_code
+            ORDER BY ts
+        ) AS vote_id
+    FROM dados
+    WHERE event_description = '{FIM_VOTO}'
+),
+votos_pareados AS (
+    SELECT 
+        i.uf,
+        i.event_date,
+        i.zone_code,
+        i.section_code,
+        i.vote_id,
+        i.inicio_voto,
+        f.fim_voto,
+        i.city_code,
+        EXTRACT(EPOCH FROM f.fim_voto - i.inicio_voto) AS duracao_segundos
+    FROM inicio i
+    JOIN fim f
+        ON i.uf = f.uf 
+        AND i.event_date = f.event_date
+        AND i.filename = f.filename
+        AND i.city_code = f.city_code
+        AND i.zone_code = f.zone_code
+        AND i.section_code = f.section_code
+        AND i.vote_id = f.vote_id
+    WHERE f.fim_voto > i.inicio_voto
+)
+SELECT 
+    uf,
+    zone_code,
+    section_code,
+    city_code,
+    AVG(duracao_segundos) AS tempo_medio_voto_segundos,
+    COUNT(*) AS total_votos
+FROM votos_pareados
+GROUP BY uf, zone_code, section_code, city_code
+ORDER BY tempo_medio_voto_segundos DESC
+LIMIT 10
 """
+
 tic=time.time()
 top_zonas = cursor.execute(query_vote_data).fetchdf()
 toc=time.time()
--- monetdblite/tempo_de_voto_por_zona.py
+++ monetdblite/tempo_de_voto_por_zona.py
@@ -1,5 +1,9 @@
 import monetdblite
 import time
+import numpy as np
+import pandas as pd  # Importa pandas para formatação
+
+# Formatando para visualizar melhor no console
 
 DB_PATH = './events_monet'
 ANCORA = 'Aguardando digitação do identificador do eleitor'
@@ -8,37 +12,38 @@ FIM_VOTO = 'O voto do eleitor foi computado'
 monetdblite.init(DB_PATH)
 con = monetdblite.connect()
 
-query = f"""SELECT event_timestamp,
-       str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S') AS FORMATED
-FROM votos_por_uf
-WHERE event_description IN ('{ANCORA}', '{FIM_VOTO}')
-LIMIT 10;"""
+query = f"""SELECT 
+  EXTRACT(DAY FROM diff_interval) * 86400 +
+  EXTRACT(HOUR FROM diff_interval) * 3600 +
+  EXTRACT(MINUTE FROM diff_interval) * 60 +
+  EXTRACT(SECOND FROM diff_interval) AS total_seconds
+FROM (
+  SELECT (TIMESTAMP '2024-10-27 16:33:51' - TIMESTAMP '2024-10-27 16:30:00') AS diff_interval
+) AS sub;
+
+"""
 
 query_vote_data= f""" 
 WITH inicio AS (
     SELECT
-        CAST(str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S') AS TIMESTAMP) AS inicio_voto,
-        uf,
-        event_date,
-        filename,
-        zone_code,
+        *,
+        str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S') AS inicio_voto,
         ROW_NUMBER() OVER (
-            PARTITION BY event_date, uf, filename, zone_code
-            ORDER BY CAST(str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S') AS TIMESTAMP)
+            PARTITION BY event_date, uf, filename
+            ORDER BY str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S')
         ) AS vote_id
     FROM votos_por_uf
     WHERE event_description = '{ANCORA}'
 ),
 fim AS (
     SELECT
-        CAST(str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S') AS TIMESTAMP) AS fim_voto,
+        str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S') AS fim_voto,
         uf,
         event_date,
         filename,
-        zone_code,
         ROW_NUMBER() OVER (
-            PARTITION BY event_date, uf, filename, zone_code
-            ORDER BY CAST(str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S') AS TIMESTAMP)
+            PARTITION BY event_date, uf, filename
+            ORDER BY str_to_timestamp(event_timestamp, '%d/%m/%Y %H:%M:%S') 
         ) AS vote_id
     FROM votos_por_uf
     WHERE event_description = '{FIM_VOTO}'
@@ -47,30 +52,35 @@ votos_completos AS (
     SELECT 
         i.uf,
         i.zone_code,
+        i.section_code,
         i.event_date,
         i.vote_id,
         i.inicio_voto,
         f.fim_voto,
-        CAST(
-            EXTRACT(SECOND FROM (f.fim_voto - i.inicio_voto)) AS DOUBLE
-        ) AS duracao_segundos
+        i.city_code,
+        EXTRACT(DAY FROM (f.fim_voto - i.inicio_voto)) * 86400 +
+        EXTRACT(HOUR FROM (f.fim_voto - i.inicio_voto)) * 3600 +
+        EXTRACT(MINUTE FROM (f.fim_voto - i.inicio_voto)) * 60 +
+        EXTRACT(SECOND FROM (f.fim_voto - i.inicio_voto)) AS duracao_segundos
     FROM inicio i
     JOIN fim f ON 
         i.vote_id = f.vote_id AND 
         i.uf = f.uf AND 
         i.event_date = f.event_date AND 
-        i.filename = f.filename AND
-        i.zone_code = f.zone_code
+        i.filename = f.filename
     WHERE f.fim_voto > i.inicio_voto         
 )
 SELECT 
     uf,
     zone_code,
+    section_code,
+    city_code,
     AVG(duracao_segundos) AS tempo_medio_voto_segundos,
     COUNT(*) AS total_votos
 FROM votos_completos
-GROUP BY uf, zone_code
+GROUP BY uf, zone_code, section_code, city_code
 ORDER BY tempo_medio_voto_segundos DESC
+LIMIT 10
 """
 
 start = time.perf_counter()
@@ -78,4 +88,7 @@ result = monetdblite.sql(query_vote_data, client=con)
 end = time.perf_counter()
 
 print(f"Consulta executada em {end - start:.2f} segundos")
-print(result)
+data = {k: v.filled() if isinstance(v, np.ma.MaskedArray) else v for k, v in result.items()}
+df = pd.DataFrame(data)
+df['tempo_medio_voto_segundos'] = df['tempo_medio_voto_segundos'].round(2)
+print(df)
